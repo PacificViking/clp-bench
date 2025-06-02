bdcc78856526cddbdac18429b8199faaad86bd7e
John Hao <johnhaoallwood@gmail.com>
2025-06-02 16:12:59 -0400

    make cache clearing worse because I don't really have that file

--- assets/dynamically-structured/clickhouse/clear-cache.sh
+++ assets/dynamically-structured/clickhouse/clear-cache.sh
@@ -3,4 +3,4 @@
 clickhouse-client --query "SYSTEM DROP UNCOMPRESSED CACHE"
 clickhouse-client --query "SYSTEM DROP MARK CACHE"
 sync
-echo 1 >/proc/sys/vm/drop_caches
+# echo 1 >/proc/sys/vm/drop_caches
