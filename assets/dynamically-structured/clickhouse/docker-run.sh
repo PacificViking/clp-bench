f5d96200f7ca5a4d82804bc191ce4abecfb948b9
John Hao <johnhaoallwood@gmail.com>
2025-06-16 15:37:58 -0400

    use /entrypoint.sh which actually loads my configurations

--- assets/dynamically-structured/clickhouse/docker-run.sh
+++ assets/dynamically-structured/clickhouse/docker-run.sh
@@ -10,6 +10,7 @@ fi
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 config_file="$script_dir/include/config.xml"
+users_config_file="$script_dir/include/users.xml"
 container_name=$(cat "$script_dir/container-name")
 workdir=/home
 
@@ -22,6 +23,7 @@ docker run \
     --name "$container_name" \
     --mount "type=bind,src=$script_dir,dst=/home/assets" \
     --mount "type=bind,src=$1,dst=/home/datasets" \
-    --mount "type=bind,src=$config_file,dst=/etc/clickhouse-server/config.d/10-benchconfig.xml" \
+    --mount "type=bind,src=$config_file,dst=/etc/clickhouse-server/config.d/benchconfig.xml" \
+    --mount "type=bind,src=$users_config_file,dst=/etc/clickhouse-server/users.d/benchconfig.xml" \
     "$container_name" \
     bash -c "cd ${workdir} && /bin/bash -l"
--- assets/dynamically-structured/clickhouse/launch.sh
+++ assets/dynamically-structured/clickhouse/launch.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 # Start the ClickHouse server in daemon mode
-clickhouse-server --daemon
+# clickhouse-server --daemon
+nohup /entrypoint.sh &
 
 # Wait until ClickHouse server is running
 while [ "$(clickhouse-client --query "SELECT 1" 2>/dev/null)" != "1" ]; do
