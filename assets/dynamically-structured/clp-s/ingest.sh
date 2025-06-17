bff1df1bfc209325a07c5f46cdf0a6ca9f9bf0e8
John Hao <johnhaoallwood@gmail.com>
2025-06-17 00:36:04 -0400

    clp-s with multiple data sources

--- assets/dynamically-structured/clp-s/ingest.sh
+++ assets/dynamically-structured/clp-s/ingest.sh
@@ -11,3 +11,4 @@ clp_s_binary=/home/assets/clp-s
 data_path=/home/archives
 
 "${clp_s_binary}" c --timestamp-key "t.\$date" --target-encoded-size 268435456 "$data_path" "$1"
+# "${clp_s_binary}" c --disable-log-order --timestamp-key "t.\$date" --target-encoded-size 268435456 "$data_path" "$1"
