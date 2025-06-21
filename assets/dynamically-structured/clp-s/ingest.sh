c7c91913c9bd4b828546f2bb5e7e8e5e3b72f245
John Hao <johnhaoallwood@gmail.com>
2025-06-21 04:41:01 -0400

    slight modifications

--- assets/dynamically-structured/clp-s/ingest.sh
+++ assets/dynamically-structured/clp-s/ingest.sh
@@ -10,5 +10,9 @@ fi
 clp_s_binary=/home/assets/clp-s
 data_path=/home/archives
 
-"${clp_s_binary}" c --timestamp-key "t.\$date" --target-encoded-size 268435456 "$data_path" "$1"
-# "${clp_s_binary}" c --disable-log-order --timestamp-key "t.\$date" --target-encoded-size 268435456 "$data_path" "$1"
+# timestamp = "t.\$date"
+timestamp="timestamp"
+# timestamp="\@timestamp"
+
+"${clp_s_binary}" c --timestamp-key "$timestamp" --target-encoded-size 268435456 "$data_path" "$1"
+# "${clp_s_binary}" c --disable-log-order --timestamp-key "$timestamp" --target-encoded-size 268435456 "$data_path" "$1"
