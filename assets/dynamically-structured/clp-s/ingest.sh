589b06d7e551266a6fc9be22690907c31da79a1f
John Hao <johnhaoallwood@gmail.com>
2025-06-28 13:08:18 -0400

    improved ingest.sh timestamp comments

--- assets/dynamically-structured/clp-s/ingest.sh
+++ assets/dynamically-structured/clp-s/ingest.sh
@@ -10,9 +10,10 @@ fi
 clp_s_binary=/clp/clp-s
 data_path=/home/archives
 
-# timestamp = "t.\$date"
-timestamp="timestamp"
-# timestamp="\@timestamp"
+# timestamp = "t.\$date"  # mongodb
+timestamp="timestamp"  # cockroachdb / postgresql
+# timestamp="\@timestamp"  # elasticsearch
+# timestamp="Timestamp"  # spark-event-logs
 
 "${clp_s_binary}" c --timestamp-key "$timestamp" --target-encoded-size 268435456 "$data_path" "$1"
 # "${clp_s_binary}" c --disable-log-order --timestamp-key "$timestamp" --target-encoded-size 268435456 "$data_path" "$1"
